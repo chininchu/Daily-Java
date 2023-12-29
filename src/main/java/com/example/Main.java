15f3d382d5699ed49063afe3eca4ccdaf2eea8a4
Andrew Chu <andrew.c.chu@outlook.com>
2023-12-29 17:14:35 -0600

    Created variables uses the Math class to check for the range in the parameter

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-
-
+        System.out.println(RgbToHex.rgb(6, 20, 500));
 
 
     }
--- src/main/java/com/example/RgbToHex.java
+++ src/main/java/com/example/RgbToHex.java
@@ -1,7 +1,22 @@
 package com.example;
 
+
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
 public class RgbToHex {
 
+    public static String rgb(int r, int g, int b) {
+
+
+        r = Math.max(0, Math.min(r, 255));
+        g = Math.max(0, Math.min(g, 255));
+        b = Math.max(0, Math.min(b, 255));
+
+
+        return null;
+
+
+    }
 
 
 }
