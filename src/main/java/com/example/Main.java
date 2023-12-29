b12547c72778344fe37a6472557ad6609ccd0f9d
Andrew Chu <andrew.c.chu@outlook.com>
2023-12-29 16:21:30 -0600

    Created the RgbToHex class

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,24 +3,9 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-//        Solution solution = new Solution();
-//
-//        System.out.println(solution.solution(5));
 
-//        System.out.println(Accumul.accum("abcd"));
-//
-//        System.out.println(Troll.disemvowel("andrew"));
 
-//        JadenCase jadenCase = new JadenCase();
-//
-//        System.out.println(jadenCase.toJadenCase(" my name is andrew"
-//        ));
 
-//        SquareDigit squareDigit = new SquareDigit();
-//
-//        System.out.println(squareDigit.squareDigits(65478909));
-
-        System.out.println(DuplicateEncoder.encode("andrewe"));
 
 
     }
