78bba478daefb8363f86f03e81fbdce740dd57fb
Andrew Chu <andrew.c.chu@outlook.com>
2024-01-04 15:01:10 -0600

    Created an if statement that parsed the value of i

--- src/main/java/com/example/DeadFish.java
+++ src/main/java/com/example/DeadFish.java
@@ -1,8 +1,40 @@
 package com.example;
 
+// Write a simple parser that will parse and run Deadfish.
+//
+//Deadfish has 4 commands, each 1 character long:
+//
+//i increments the value (initially 0)
+//d decrements the value
+//s squares the value
+//o outputs the value into the return array
+//Invalid characters should be ignored.
+
 public class DeadFish {
 
     public static int[] parse(String data) {
+
+        int val = 0;
+
+        for (int i = 0; i < data.length(); i++) {
+
+            char ch = data.charAt(i);
+
+
+
+            if(ch == 'i'){
+
+                val += i;
+
+
+            }
+
+
+
+
+
+
+        }
 
 
         return null;
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println(RgbToHex.rgb(255, 255, 255));
+//        System.out.println(RgbToHex.rgb(255, 255, 255));
+//
+//        System.out.println(Kata.createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 7, 4, 2}));
 
-        System.out.println(Kata.createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 7, 4, 2}));
+        System.out.println(DeadFish.parse("iiisdoso"));
+
 
 
     }
