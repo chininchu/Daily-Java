0119b3fceb3f8983658a99e7683bc44d846be621
Andrew Chu <andrew.c.chu@outlook.com>
2024-01-08 20:04:47 -0600

    Completed the code within the ASum class

--- src/main/java/com/example/ASum.java
+++ src/main/java/com/example/ASum.java
@@ -1,12 +1,37 @@
 package com.example;
 
 
+import java.util.*;
+import java.util.function.BinaryOperator;
+
 public class ASum {
 
     public static long findNb(long m) {
-        // your code
+
+        long sum = 0;
+        long n = 0;
+
+        while (sum < m) {
+
+            n++;
+            sum += n * n * n;
+
+
+        }
+
+        if (sum == m) {
+            return n;
+
+
+        } else {
+
+            return -1;
+
+
+        }
 
 
     }
+
 
 }
