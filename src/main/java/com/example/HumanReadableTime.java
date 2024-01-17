0c89db90a04cda11fd8686d33a560a4a68a69d97
Andrew Chu <andrew.c.chu@outlook.com>
2024-01-17 07:37:19 -0600

    Created a HumanReadableTime

---
+++ src/main/java/com/example/HumanReadableTime.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class HumanReadableTime {
+
+    public static String makeReadable(int seconds){
+
+
+
+
+
+
+        return Integer.toString(seconds);
+
+
+
+
+    }
+
+}
